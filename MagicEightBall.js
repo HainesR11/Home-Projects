992e75b9bdb593583ecab4c073feaae0fa1f8095
HainesR11 <58213900+HainesR11@users.noreply.github.com>
2021-03-20 23:57:38 +0000

    Add files via upload

---
+++ MagicEightBall.js
@@ -0,0 +1,34 @@
+var userName = "";
+userName === "" ? userName = "John" : userName = userName;
+console.log(userName) ;
+const userQuestion = "Am i right?"
+console.log(userQuestion);
+var randomNumber = Math.floor(Math.random() * 8);
+var eightBall = "";
+switch (randomNumber){
+  case 1:
+  eightBall = "It is certain";
+  break;
+  case 1:
+  eightBall = "It is decidedly so";
+  break;
+  case 2:
+  eightBall = "Reply hazy try again";
+  break;
+  case 3:
+  eightBall = "Cannot predict now"
+  break;
+  case 4:
+  eightBall = "Do not count on it";
+  break;
+  case 5:
+  eightBall = "My sources say no";
+  break;
+  case 6:
+  eightBall = "Outlook not so good";
+  break;
+  case 7:
+  eightBall = "Signs point to yes";
+  break;
+}
+console.log(eightBall);
